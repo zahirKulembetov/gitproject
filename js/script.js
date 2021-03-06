e3b5c4657a5d17638f53f059f2aac849011f9118
Zahir <kul-135@mail.ru>
2021-03-06 18:28:44 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let cont = true;
+
+const obg = {
+   true: 10,
+   left: 122,
+   kutak: 22
+};
+
+console.log(obg.kutak);
+
+let massive = ['rr', 'efe', 34, [], {}];
+
+console.log(massive[3]);
